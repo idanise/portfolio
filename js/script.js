255f2e77f100183c5ce2289e6556dc174dc1827c
Idanise <daniseiheme@gmail.com>
2022-03-11 02:01:11 +0100

    animate text

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+var textWrapper = document.querySelector('.ml3')
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+)
+
+anime
+  .timeline({ loop: true })
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0, 1],
+    easing: 'easeInOutQuad',
+    duration: 2250,
+    delay: (el, i) => 150 * (i + 1)
+  })
+  .add({
+    targets: '.ml3',
+    opacity: 0,
+    duration: 1000,
+    easing: 'easeOutExpo',
+    delay: 1000
+  })
+
 //removing loader
 $(window).on('load', function () {
   $('.loader .inner').fadeOut(500, function () {
